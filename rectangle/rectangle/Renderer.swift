7f2a83d71618c9a1da727592372c51288a32b68a
pino <uni.nova.tih@gmail.com>
2026-02-13 22:37:00 +0100

    fixed issue

--- rectangle/rectangle/Renderer.swift
+++ rectangle/rectangle/Renderer.swift
@@ -52,17 +52,16 @@ final class Renderer: NSObject {
         renderPipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
         
         let vertexDescriptor = MTLVertexDescriptor()
-        vertexDescriptor.attributes[0].format = .float4
+        vertexDescriptor.attributes[0].format = .float3
         vertexDescriptor.attributes[0].offset = 0
         vertexDescriptor.attributes[0].bufferIndex = 0
         vertexDescriptor.attributes[1].format = .float4
-        vertexDescriptor.attributes[1].offset = MemoryLayout<SIMD4<Float>>.stride
+        vertexDescriptor.attributes[1].offset = MemoryLayout<SIMD3<Float>>.stride
         vertexDescriptor.attributes[1].bufferIndex = 0
         vertexDescriptor.layouts[0].stride = MemoryLayout<Vertex>.stride
         renderPipelineDescriptor.vertexDescriptor = vertexDescriptor
         
         renderPipelineState = try! device.makeRenderPipelineState(descriptor: renderPipelineDescriptor)
-        
         
         
         vertexBuffer = device.makeBuffer(bytes: vertices, length: MemoryLayout<Vertex>.stride * vertices.count)
