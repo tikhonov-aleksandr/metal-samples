b989c2956b7a301b8a22bb1582f7f535535147bb
pino <uni.nova.tih@gmail.com>
2026-03-31 22:13:45 +0200

    template code for texture

--- texture/texture/Renderer.swift
+++ texture/texture/Renderer.swift
@@ -14,6 +14,21 @@ final class Renderer: NSObject {
     private var commandQueue: MTLCommandQueue!
     private var renderPipelineState: MTLRenderPipelineState!
     
+    private var vertexBuffer: MTLBuffer!
+    private var indexBuffer: MTLBuffer!
+    
+    private var vertices: [Vertex] = [
+        Vertex(position: SIMD3<Float>(-1, 1, 0), color: SIMD4<Float>(1, 0, 0, 1)), // (0)
+        Vertex(position: SIMD3<Float>(-1, -1, 0), color: SIMD4<Float>(0, 1, 0, 1)), // (1)
+        Vertex(position: SIMD3<Float>(1, -1, 0), color: SIMD4<Float>(0, 0, 1, 1)), // (2)
+        Vertex(position: SIMD3<Float>(1, 1, 0), color: SIMD4<Float>(0, 0, 0, 1)) // (3)
+    ]
+    
+    private var indices: [UInt16] = [
+        0, 1, 2,
+        0, 2, 3
+    ]
+    
     override init() {
         super.init()
         setup()
@@ -34,21 +49,53 @@ final class Renderer: NSObject {
         let library = device.makeDefaultLibrary()
         let vertexFunction = library?.makeFunction(name: "vertex_main")
         let fragmentFunction = library?.makeFunction(name: "fragment_main")
-        
         renderPipelineDescriptor.vertexFunction = vertexFunction
         renderPipelineDescriptor.fragmentFunction = fragmentFunction
+        renderPipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        
+        let vertexDescriptor = MTLVertexDescriptor()
+        vertexDescriptor.attributes[0].format = .float3
+        vertexDescriptor.attributes[0].offset = MemoryLayout<Vertex>.offset(of: \.position)!
+        vertexDescriptor.attributes[0].bufferIndex = 0
+        vertexDescriptor.attributes[1].format = .float4
+        vertexDescriptor.attributes[1].offset = MemoryLayout<Vertex>.offset(of: \.color)!
+        vertexDescriptor.attributes[1].bufferIndex = 0
+        vertexDescriptor.layouts[0].stride = MemoryLayout<Vertex>.stride
+        renderPipelineDescriptor.vertexDescriptor = vertexDescriptor
         
         renderPipelineState = try! device.makeRenderPipelineState(descriptor: renderPipelineDescriptor)
+        vertexBuffer = device.makeBuffer(bytes: vertices, length: MemoryLayout<Vertex>.stride * vertices.count)
+        indexBuffer = device.makeBuffer(bytes: indices, length: MemoryLayout<UInt16>.stride * indices.count)
     }
     
+    private func drawCommands(in view: MTKView) {
+        guard
+            let currentDrawable = view.currentDrawable,
+            let currentRenderPassDescriptor = view.currentRenderPassDescriptor
+        else { return }
+        
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        let renderCommandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: currentRenderPassDescriptor)
+        renderCommandEncoder?.setRenderPipelineState(renderPipelineState)
+        renderCommandEncoder?.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+        renderCommandEncoder?.drawIndexedPrimitives(
+            type: .triangle,
+            indexCount: indices.count,
+            indexType: .uint16,
+            indexBuffer: indexBuffer,
+            indexBufferOffset: 0
+        )
+        
+        renderCommandEncoder?.endEncoding()
+        commandBuffer?.present(currentDrawable)
+        commandBuffer?.commit()
+    }
 }
 
 extension Renderer: MTKViewDelegate {
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) { }
     
     func draw(in view: MTKView) {
-        
+        drawCommands(in: view)
     }
-    
-    
 }
---
+++ texture/texture/Vertex.swift
@@ -0,0 +1,13 @@
+//
+//  Vertex.swift
+//  texture
+//
+//  Created by pino on 31.03.26.
+//
+
+import Foundation
+
+struct Vertex {
+    var position: SIMD3<Float>
+    var color: SIMD4<Float>
+}
